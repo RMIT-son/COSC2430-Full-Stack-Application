6f7dc02168967d3ba86562e6ccf1c59eddb383ed
RMIT-son <119044913+RMIT-son@users.noreply.github.com>
2023-09-19 09:42:53 +0700

    added some base code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const {static} = require("express");
 const app = express();
 
 // Connect to MongoDB
@@ -9,6 +10,8 @@ mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true })
 
 // Set up EJS as the view engine
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
 
 // Set up routes
 app.get('/', (req, res) => {
@@ -16,5 +19,5 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
